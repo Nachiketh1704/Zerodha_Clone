0fa64441ff806e99e4335cf64a6c96a6ce9cda96
Nachiketh <bellamkonda.nachikethgupta@gmail.com>
2025-06-24 01:00:24 +0530

    Completed backend and dashboard

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const { HoldingModel } = require("./model/HoldingModel");
+const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/allHoldings", async (req, res) => {
+  let allHoldings = await HoldingModel.find({});
+  res.json(allHoldings);
+});
+
+app.get("/allPositions", async (req, res) => {
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+});
+
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+
+  newOrder.save();
+
+  res.send("Order saved!");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:3000");
+  mongoose.connect(MONGO_URI);
+  mongoose.connection.on("error", (err) => {
+    console.error("MongoDB connection error:", err);
+  });
+});
